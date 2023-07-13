eaee886c691c885ffc4de47ed82eb682dcc73809
ArtemDerenok <derenokartem95@gmail.com>
2023-07-13 16:38:19 +0300

    feat: add full validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './scss/style.scss';
 const btn = document.getElementById('btn');
 const form = document.getElementById('form');
 const inputs = document.querySelectorAll('.form__input');
+const errorMsg = document.getElementById('error-msg');
 
 const clearErrorMessages = () => {
   inputs.forEach((elem) => {
@@ -13,9 +14,16 @@ const clearErrorMessages = () => {
     copyElem.nextElementSibling.innerText = '';
     copyElem.previousElementSibling.classList.remove('error');
   });
+  errorMsg.classList.remove('error');
+  errorMsg.innerText = '';
 };
 
 const handleValidationResult = (result) => {
+  if (!result.date.isValid) {
+    errorMsg.classList.add('error');
+    errorMsg.innerText = result.date.message;
+  }
+
   inputs.forEach((elem) => {
     if (!result[elem.name].isValid) {
       const copyElem = elem;
--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,9 +1,28 @@
 class Validate {
-  #checkData(value) {
+  #checkData(value, name) {
     const data = value.trim();
-
     if (data === '') {
       return { isValid: false, message: 'This field is required' };
+    }
+    if (!+data && +data !== 0) {
+      return { isValid: false, message: 'This value must be a number' };
+    }
+    if (name === 'day' && (+data < 1 || +data > 31)) {
+      return { isValid: false, message: 'The day number is not between 1-31' };
+    }
+    if (name === 'month' && (+data < 1 || +data > 12)) {
+      return { isValid: false, message: 'The month number is not between 1-12' };
+    }
+    if (name === 'year' && +data > new Date().getFullYear()) {
+      return { isValid: false, message: 'The year is in the future' };
+    }
+
+    return { isValid: true };
+  }
+
+  #checkDate(date, monthNum) {
+    if (date.getMonth() + 1 !== monthNum) {
+      return { isValid: false, message: 'The date is invalid' };
     }
     return { isValid: true };
   }
@@ -11,16 +30,29 @@ class Validate {
   validate(elements) {
     const result = {
       result: true,
+      date: {},
     };
 
     for (let i = 0; i < elements.length; i += 1) {
-      result[elements[i].name] = this.#checkData(elements[i].value);
+      result[elements[i].name] = this.#checkData(elements[i].value, elements[i].name);
 
       if (result[elements[i].name].isValid === false) {
         result.result = false;
       }
     }
 
+    if (result.result) {
+      const dateResult = this.#checkDate(
+        new Date(+elements.year.value, +elements.month.value - 1, +elements.day.value),
+        +elements.month.value,
+      );
+
+      if (!dateResult.isValid) {
+        result.result = false;
+      }
+
+      result.date = dateResult;
+    }
     return result;
   }
 }
